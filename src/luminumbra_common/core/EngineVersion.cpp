6bdfa2e8ca613583fcaf8f4be1fb0fe790ce0a7a
David Addison <73078380+d-addison@users.noreply.github.com>
2026-06-07 13:49:28 -0400

    T-OD4-embed-git-sha-engine-version: Embed git SHA in engine version
    Forge-Task-Id: T-OD4-embed-git-sha-engine-version
    Forge-Attempt-Id: T-OD4-embed-git-sha-engine-version
    (cherry picked from commit 071c36f6d28addab243e10b4bc02661726886077)

---
+++ src/luminumbra_common/core/EngineVersion.cpp
@@ -0,0 +1,20 @@
+#include "core/EngineVersion.h"
+
+namespace luminumbra::core {
+
+std::string_view GetEngineVersion() noexcept
+{
+    return kEngineVersion;
+}
+
+std::string_view GetEngineGitSha() noexcept
+{
+    return kEngineGitSha;
+}
+
+std::string_view GetEngineVersionString() noexcept
+{
+    return kEngineVersionString;
+}
+
+} // namespace luminumbra::core
